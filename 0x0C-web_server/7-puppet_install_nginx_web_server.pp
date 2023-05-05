15dec0dd8c1327e0f529e79b24516e007885227b
WhizIkem <wisdomikem@gmail.com>
2023-05-04 17:06:40 -0700

    update on tasks

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,24 @@
+# install and configure server using puppet
+
+package { 'nginx':
+  ensure   => installed,
+  name     => 'nginx'
+}
+
+file { '/var/www/html/index.html' :
+  content   => 'Hello World!',
+  path      => '/var/www/html/index.html'
+}
+
+file_line { 'title' :
+  ensure    => present,
+  path      => '/etc/nginx/sites-available/default',
+  after     => 'server_name _;',
+  line      => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  mulitple  => true
+}
+
+service { 'nginx':
+  ensure   => running,
+  require  => Package['nginx'],
+}
