ddc60c4596c578dc466c7d4b64c110eb000b7104
WhizIkem <wisdomikem@gmail.com>
2023-05-06 12:43:30 -0700

    puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -11,11 +11,10 @@ file { '/var/www/html/index.html' :
 }
 
 file_line { 'title' :
-  ensure    => present,
+  ensure    => 'present',
   path      => '/etc/nginx/sites-available/default',
-  after     => 'server_name _;',
+  after     => 'listen 80 default_server;',
   line      => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-  mulitple  => true
 }
 
 service { 'nginx':
