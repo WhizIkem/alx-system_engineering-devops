852d9a2ae3d8b9eebdd1669c23448ea503749a13
whizikem <wisdomikem@gmail.com>
2023-06-11 23:15:06 +0000

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,35 +10,31 @@ import sys
 
 def get_employee_todo_progress(employee_id):
     base_url = 'https://jsonplaceholder.typicode.com'
+
+    # Retrieve employee information
     employee_url = '{}/users/{}'.format(base_url, employee_id)
+    response = requests.get(employee_url)
+    employee_data = response.json()
+    employee_name = employee_data['name']
+
+    # Retrieve employee's TODO list
     todos_url = '{}/todos?userId={}'.format(base_url, employee_id)
+    response = requests.get(todos_url)
+    todos_data = response.json()
 
-    try:
-        employee_response = requests.get(employee_url)
-        todos_response = requests.get(todos_url)
+    # Calculate TODO list progress
+    completed_tasks = [task for task in todos_data if task['completed']]
+    number_of_done_tasks = len(completed_tasks)
+    total_number_of_tasks = len(todos_data)
 
-        employee_response.raise_for_status()
-        todos_response.raise_for_status()
+    # Print employee TODO list progress
+    print('Employee {} is done with tasks ({}/{}):'.format(employee_name,
+          number_of_done_tasks, total_number_of_tasks))
+    print('{}: {}/{}'.format(employee_name, number_of_done_tasks,
+                             total_number_of_tasks))
 
-        employee_data = employee_response.json()
-        todos_data = todos_response.json()
-
-        employee_name = employee_data['name']
-
-        completed_tasks = [task for task in todos_data if task['completed']]
-        number_of_done_tasks = len(completed_tasks)
-        total_number_of_tasks = len(todos_data)
-
-        print('Employee {} is done with tasks ({}/{}):'.format(employee_name,
-              number_of_done_tasks, total_number_of_tasks))
-        print('{}: {}/{}'.format(employee_name, number_of_done_tasks,
-              total_number_of_tasks))
-
-        for task in completed_tasks:
-            print('\t{}'.format(task['title']))
-
-    except requests.exceptions.RequestException as e:
-        print('Error: {}'.format(e))
+    for task in completed_tasks:
+        print('\t{}'.format(task['title']))
 
 
 if __name__ == '__main__':
