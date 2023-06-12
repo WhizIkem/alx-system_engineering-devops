ed1001a6221b39a48f962786ccb69837d2d3c1c9
whizikem <wisdomikem@gmail.com>
2023-06-12 10:27:38 +0000

    Export to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -3,9 +3,9 @@
 Using what you did in the task #0, extend your Python script to export data
 in the CSV format.
 """
+import csv
 import requests
 import sys
-import csv
 
 
 def get_employee_todo_progress(employee_id):
