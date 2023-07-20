f16dd9c22e2732b77dbb9d2e71e87c5b6994bd02
whizikem <wisdomikem@gmail.com>
2023-07-20 16:34:43 +0100

    count it

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,61 +1,48 @@
 #!/usr/bin/python3
+
+import requests
+
 """ Count it! """
-from requests import get
+def count_words(subreddit, word_list, after=None, word_count={}):
+    """Recursive function to count words in hot articles on Reddit"""
 
-REDDIT = "https://www.reddit.com/"
-HEADERS = {'user-agent': 'my-app/0.0.1'}
+    # Base case: if word_list is empty, print the results in descending order
+    if not word_list:
+        sorted_counts = sorted(word_count.items(), key=lambda x: (-x[1], x[0]))
+        for word, count in sorted_counts:
+            print(f"{word}: {count}")
+        return
 
+    # If word_list is not empty, take the first word and remove it from the list
+    current_word = word_list[0].lower()
+    remaining_words = word_list[1:]
 
-def count_words(subreddit, word_list, after="", word_dic={}):
-    """
-    Returns a list containing the titles of all hot articles for a
-    given subreddit. If no results are found for the given subreddit,
-    the function should return None.
-    """
-    if not word_dic:
-        for word in word_list:
-            word_dic[word] = 0
+    # If the current_word is empty or None, stop the recursion for this word
+    if not current_word:
+        count_words(subreddit, remaining_words, after, word_count)
+        return
 
-    if after is None:
-        word_list = [[key, value] for key, value in word_dic.items()]
-        word_list = sorted(word_list, key=lambda x: (-x[1], x[0]))
-        for w in word_list:
-            if w[1]:
-                print("{}: {}".format(w[0].lower(), w[1]))
-        return None
+    # Fetch hot articles from Reddit using the provided subreddit
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {"User-Agent": "My User Agent"}  # Add your user agent here
+    params = {"limit": 100, "after": after}
+    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
 
-    url = REDDIT + "r/{}/hot/.json".format(subreddit)
+    # If the subreddit is invalid or no more articles to fetch, stop the recursion
+    if response.status_code != 200:
+        return
 
-    params = {
-        'limit': 100,
-        'after': after
-    }
+    # Get the data from the API response
+    data = response.json().get("data")
+    if not data:
+        return
 
-    r = get(url, headers=HEADERS, params=params, allow_redirects=False)
+    # Extract the titles from the data and count the occurrences of the current_word
+    articles = data.get("children", [])
+    for article in articles:
+        title = article.get("data", {}).get("title", "").lower()
+        word_count[current_word] = word_count.get(current_word, 0) + title.count(current_word)
 
-    if r.status_code != 200:
-        return None
+    # Recursively call the function with the remaining words and continue fetching articles
+    count_words(subreddit, remaining_words, data.get("after"), word_count)
 
-    try:
-        js = r.json()
-
-    except ValueError:
-        return None
-
-    try:
-
-        data = js.get("data")
-        after = data.get("after")
-        children = data.get("children")
-        for child in children:
-            post = child.get("data")
-            title = post.get("title")
-            lower = [s.lower() for s in title.split(' ')]
-
-            for w in word_list:
-                word_dic[w] += lower.count(w.lower())
-
-    except:
-        return None
-
-    count_words(subreddit, word_list, after, word_dic)
